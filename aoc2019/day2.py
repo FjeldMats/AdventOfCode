0720f4e50aabd61699c64927caef568099da29b7
FjeldMats <FjeldMats@gmail.com>
2021-12-04 17:36:48 +0100

    finish day 2

--- aoc2019/day2.py
+++ aoc2019/day2.py
@@ -1,34 +1,51 @@
 
-nums = list(map(int, open("aoc2019/2.txt").read().split(",")))
+nums = list(map(int, open("aoc2019/inputs/2.txt").read().split(",")))
 
-# restore the computer to "1202 program alarm"
-nums[1] = 12
-nums[2] = 2
-
-# optcode always at index 0
-# - can be three values: 
-#      - optcode 1: add numbers
-#      - optcode 2: multiply numbers 
-#      - optcode 3: program halt 
 
 def evalOptcode(lst, row):
-    # if first index 1 add,  otherwise mult
     if lst[row] == 1:
         return lst[lst[row+1]] + lst[lst[row+2]]
-    else: 
-
+    else:
         return lst[lst[row+1]] * lst[lst[row+2]]
 
 
 def checkHealt(lst, row):
     return lst[row] == 99
 
-row = 0
-while(checkHealt(nums, row) == False):
-    newNum = evalOptcode(nums, row)
-    nums[row+3] = newNum
-    print(nums[row:row+4])
-    row += 4
+
+def run_computer(nums, noun=None, verb=None):
+    if noun != None:
+        nums[1] = noun
+    if verb != None:
+        nums[2] = verb
+
+    row = 0
+    while(checkHealt(nums, row) == False):
+        newNum = evalOptcode(nums, row)
+        new_idx = nums[row+3]
+        if new_idx > len(nums):
+            return -1  # invalid access
+        nums[new_idx] = newNum
+        row += 4
+    return nums[0]
 
 
-print(nums[row-4:row])
+def search_for_output(nums, output, search_space):
+    for noun in range(search_space):
+        for verb in range(search_space):
+            program = nums.copy()
+            x = run_computer(program, noun, verb)
+            if x == output:
+                return noun, verb
+    return -1, -1
+
+# part 1
+
+
+value = run_computer(nums.copy(), 12, 2)
+print(value)
+
+# part 2
+noun, verb = search_for_output(nums.copy(), 19690720, len(nums))
+print(noun, verb)
+print(100 * noun + verb)
